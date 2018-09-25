507fc054ba707249e10c43c666a265b70399841a
Santoshkumar Yadav <syadav214@gmail.com>
2018-09-25 11:28:25 +0530

    MAPS STL

---
+++ Hackerrank_cpp/stl/Maps-STL.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    map<string, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        int type;
+        string X;
+        cin >> type >> X;
+        switch (type)
+        {
+        case 1:
+            int Y;
+            cin >> Y;
+            if (m.count(X) > 0)
+            {
+                Y += m[X];
+                // m[X] +=Y;
+            }
+
+            /* We can find a key using below code
+             map<string,int>::iterator itr = m.find(X);
+              if(itr == m.end())
+              {
+                  //NOT FOUND
+              }
+            */
+
+            m[X] = Y;
+            //m.insert({X,Y});
+            break;
+        case 2:
+            m.erase(X);
+            break;
+        case 3:
+            if (m.count(X) == 0)
+            {
+                cout << 0 << endl;
+            }
+            else
+            {
+                cout << m[X] << endl;
+            }
+            break;
+        }
+    }
+
+    return 0;
+}
